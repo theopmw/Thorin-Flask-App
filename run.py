f3f002b2a40bd06115a3c81f8c51a7c0963d2a5d
theopmw <theopmw@gmail.com>
2021-06-11 11:56:41 +0000

    Use for loop to iterate through about data and render chatacter data in about.html

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 # Import os from standard Python library
 import os
+import json
 # Import Flask class
 from flask import Flask, render_template
 
@@ -17,7 +18,10 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html", page_title="About")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/contact")
